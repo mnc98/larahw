5f509ca2dbac25ad1df7537c7156b1880b0e8ce1
mnc98 <mncohen45@gmail.com>
2019-03-29 16:31:40 -0400

    Added PageController with routing to home, about, contact us and login pages (2nd attempt)

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function home() {
+        return view('pages.home');
+    }
+    function about() {
+        return view('pages.about');
+    }
+    function contact() {
+        return view('pages.contact');
+    }
+    function login() {
+        return view('pages.login');
+    }
+}
